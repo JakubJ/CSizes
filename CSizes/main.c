dc20bf127dae09703d56adb486b5a353a14bba70
jak-open <jakub@Jakub-Mac-mini.local>
2013-08-28 19:37:22 +0200

    Uvodni commit

--- CSizes/main.c
+++ CSizes/main.c
@@ -8,11 +8,29 @@
 
 #include <stdio.h>
 
+
 int main (int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    printf ("Size of int: %lu \n", sizeof(int));
+    printf ("Size of char: %lu \n", sizeof(char));
+    printf ("Size of float: %lu \n", sizeof(float));
+    printf ("Size of double: %lu \n", sizeof(double));
+    
+    printf ("Size of short int: %lu \n", sizeof(short int));
+    printf ("Size of long double: %lu \n", sizeof(long double));
+
+    
+    int i = 0;
+    printf("i: %i\n",i);
+    printf("i++: %i\n",i++);
+    printf("i: %i\n\n",i);
+    
+    i=0;
+    printf("i: %i\n",i);
+    printf("++i: %i\n",++i);
+    printf("i: %i\n",i);
+
     return 0;
 }
 
